ca38ec915c5b6887315af8aa1df69fce286e0b8b
hiepnxph <hiepnxph@gmail.com>
2022-01-13 13:59:32 +0700

    uplate

--- src/page/adminew.js
+++ src/page/adminew.js
@@ -142,9 +142,11 @@ const AdminNews = {
             </h1>
           </div>
         </header>
+        <a href="/admin/news/add">
         <button type="submit" class=" mr-10 mt-10 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    <a href="/admin/news/add">Add</a>
+                   Add
                   </button>
+                  </a>
         <main>
           <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
             <!-- Replace with your content -->
